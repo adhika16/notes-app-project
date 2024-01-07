aa5245bdc32f64105887caf73a63a712d5963889
Adhika P <40765693+adhika16@users.noreply.github.com>
2024-01-07 14:41:50 +0700

    [.net] Create GetUserIdFilterAttribute.cs
    this attribute is created to get userId from accesstoken for scope purposes

---
+++ notes-app-server-net/notes-app-server-net/Attributes/GetUserIdFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace notes_app_server_net.Attributes
+{
+    public class GetUserIdFilterAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // Access the user's claims from the AuthorizationHandlerContext
+            var claims = context.HttpContext.User.Claims;
+
+            // Extract the userId or sub claim
+            string userIdOrSub = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier || c.Type == "sub")?.Value;
+
+            // Store the userId or sub in the HttpContext items dictionary
+            context.HttpContext.Items["userIdOrSub"] = userIdOrSub;
+        }
+    }
+}
